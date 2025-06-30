5e9a6079041764e38b6d41b6b5b421581867d105
TriesteCitizen <lorenzo.wendland18@gmail.com>
2025-06-30 14:28:08 +0200

    Fixed Singleton class

--- src/creational/MySingleton.java
+++ src/creational/MySingleton.java
@@ -1,5 +1,19 @@
 package creational;
 
 public class MySingleton {
+	private static volatile MySingleton instance;
+	
+	private MySingleton() {}
 
+	public static synchronized MySingleton getInstance() {
+		if(instance == null) {
+			synchronized(MySingleton.class) {
+				if(instance == null) {
+					instance = new MySingleton();
+				}
+			}
+		}
+		return instance;
+	}
 }
+//how could we fix problems around double-checked locking
